fa0f69c5c2366eb64db0ecfa2b25faebea8df108
Angwoo <jeeyoungwoo1027@gmail.com>
2022-03-14 09:30:16 +0900

    angwoos first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:demo/custom_icon.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: 기록',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: 분석',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: 추천',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: 마이페이지',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("식단 로그를 입력하세요!")),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CustomIcon.edit),
+            label: '기록',
+            backgroundColor: Colors.lightGreen,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CustomIcon.chart_bar),
+            label: '분석',
+            backgroundColor: Colors.lightGreen,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CustomIcon.thumbs_up),
+            label: '추천',
+            backgroundColor: Colors.lightGreen,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '내 정보',
+            backgroundColor: Colors.lightGreen,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+        elevation: 0,
+      ),
+    );
+  }
+}
