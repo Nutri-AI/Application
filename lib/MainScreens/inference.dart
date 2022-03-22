18cf71e785e94ef5697fc2ebfaa7dcf83f8bca76
hyeniii <hyewonee96@gmail.com>
2022-03-22 14:16:53 +0900

    transfered inference result from food_log to inference dart file

--- lib/MainScreens/inference.dart
+++ lib/MainScreens/inference.dart
@@ -2,19 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class Inference extends StatefulWidget {
-  Inference({Key? key}) : super(key: key);
+  String uri;
+  List<dynamic>? subcat;
+  Inference({Key? key, required this.uri, this.subcat}) : super(key: key);
 
   @override
   State<Inference> createState() => _InferenceState();
 }
 
 class _InferenceState extends State<Inference> {
+  late String url;
+  late List<dynamic>? foodList;
+
+  @override
+  void initState() {
+    url = widget.uri;
+    foodList = widget.subcat;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: const Text("웰컴!!!!!!!!"),
-      ),
+      body: Container(child: Text(url)),
     );
   }
 }
