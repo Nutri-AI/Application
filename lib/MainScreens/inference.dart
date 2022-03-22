1429b1afd8171252290adc576322c0497e3efee7
Angwoo <jeeyoungwoo1027@gmail.com>
2022-03-22 14:18:36 +0900

    modified inference.dart

--- lib/MainScreens/inference.dart
+++ lib/MainScreens/inference.dart
@@ -9,11 +9,23 @@ class Inference extends StatefulWidget {
 }
 
 class _InferenceState extends State<Inference> {
+  List class_type = ["비빔밥", "김치찌개"];
+  List food_list = [
+    ["돌솥비빔밥", "멍게비빔밥", "비빔밥", "약초비빔밥", "육회비빔밥"],
+    ["김치찌개", "돼지김치찌개", "꽁치김치찌개", "참치김치찌개"]
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: const Text("웰컴!!!!!!!!"),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              for (int i = 0; i < class_type.length; i++) Text(class_type[i])
+            ],
+          ),
+        ),
       ),
     );
   }
